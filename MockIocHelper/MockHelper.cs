150abf9527a7e38e7d344443250c209abd7df8fc
garrypas <Garry.Passarella@VERIFILE-DEV14.verifile.local>
2015-06-10 11:17:59 +0100

    init repo

---
+++ MockIocHelper/MockHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Moq;
+
+namespace MockIocHelper
+{
+    public class MockHelper
+    {
+        private readonly IDictionary<Type, Mock> _mocks;
+
+        public MockHelper()
+        {
+            _mocks = new Dictionary<Type, Mock>();
+        }
+
+        public T Object<T>()
+            where T : class
+        {
+            return GetMock<T>().Object;
+        }
+
+        public Mock<T> GetMock<T>()
+            where T : class
+        {
+            var key = typeof(T);
+            return (Mock<T>)(_mocks.ContainsKey(key) ? _mocks[key] : _mocks[key] = new Mock<T>());
+        }
+
+        public T Create<T>(params object[] overrides)
+            where T : class
+        {
+            var largestCtor = typeof(T).GetConstructors().Select(c => new
+            {
+                ParameterCount = c.GetParameters().Count(),
+                ConstructorInfo = c
+            }).OrderByDescending(c => c.ParameterCount).First().ConstructorInfo;
+            var parms = largestCtor.GetParameters();
+            var objects = new List<object>();
+            foreach (var p in parms)
+            {
+                var matchedInOverrides = GetMatchFromOverrides(overrides, p.ParameterType);
+                if (matchedInOverrides != null)
+                {
+                    objects.Add(matchedInOverrides);
+                    continue;
+                }
+                GetMethod("GetMock", p.ParameterType).Invoke(this, new object[] { });
+                objects.Add(GetMethod("Object", p.ParameterType).Invoke(this, new object[] { }));
+            }
+            return (T)largestCtor.Invoke(objects.ToArray());
+        }
+
+        private static object GetMatchFromOverrides(object[] overrides, Type t)
+        {
+            return overrides.FirstOrDefault(o => o.GetType().IsSubclassOf(t) || o.GetType() == t || o.GetType().GetInterfaces().Any(i => i == t));
+        }
+
+        private MethodInfo GetMethod(string methodName, Type t)
+        {
+            return GetType().GetMethod(methodName, BindingFlags.Instance | BindingFlags.Public, null, new Type[] { }, null).MakeGenericMethod(t);
+        }
+    }
+}
---
+++ MockIocHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("MockIocHelper")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Garry Passarella")]
+[assembly: AssemblyProduct("MockIocHelper")]
+[assembly: AssemblyCopyright("Copyright © Garry Passarella 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2ef65a0f-8b24-4cac-9b36-dad9fada7c89")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
